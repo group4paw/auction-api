6a98197a5f73cefb91b15ef34650d10318ce8b49
Aly Murry Darmoko <133185520+amurryd@users.noreply.github.com>
2023-09-20 17:29:50 +0700

    Update Wishlist.js
    Added getPainting() method for the wishlist

--- api/models/Wishlist.js
+++ api/models/Wishlist.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const wishlistSchema = new mongoose.Schema({
-  idWishlist: { type: String, unique: true, required: true },
-  idUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  paintings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Painting' }],
+  idWishlist: { type: Number, unique: true, required: true },
+  idUser: { type: Number, required: true },
 });
+
+wishlistSchema.methods.getPainting = async function (paintingId) {
+  const WishlistItem = mongoose.model('WishlistItem');
+  const item = await WishlistItem.findOne({ idWishlist: this.idWishlist, idPainting: paintingId });
+  if (!item) {
+    return null;
+  }
+  return item.toObject(); // Convert to plain JavaScript object or the desired type
+};
 
 module.exports = mongoose.model('Wishlist', wishlistSchema);
