92fc3aa36539f6918ff39233f7afa0814ea5d0e4
hieraar <hiera.ahm2002@mail.ugm.ac.id>
2023-09-23 00:50:10 +0700

    Menambahkan insurance

--- index.js
+++ index.js
@@ -26,10 +26,12 @@ app.listen(3000, () => {
 const UserRouter = require("./api/routes/User.js");
 const CustomerRouter = require("./api/routes/customerRoutes.js");
 const SellerRouter = require("./api/routes/sellerRoutes.js");
-const Delivery = require("./api/routes/deliveryRoutes.js")
+const Delivery = require("./api/routes/deliveryRoutes.js");
+const Insurance = require("./api/routes/insuranceRoutes.js");
 
 app.use("/user", UserRouter);
 app.use("/customer", CustomerRouter);
 app.use("/seller", SellerRouter);
-app.use("", Delivery);
+
+app.use("", Insurance);
 
