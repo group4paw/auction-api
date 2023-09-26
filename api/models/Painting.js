5fc36f9eeaa70a0ac9221e72693346c6718c6019
Aly Murry Darmoko <133185520+amurryd@users.noreply.github.com>
2023-09-26 16:11:16 +0700

    Update and rename Painting.js to paintingModel.js

--- api/models/Painting.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const paintingSchema = new mongoose.Schema({
-  title: String,
-  // Add other painting-related fields as needed
-});
-
-module.exports = mongoose.model('Painting', paintingSchema);
