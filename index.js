5cb88850be85435530fd810d6ac8f5987f85adc1
Farhan Franaka <farhanfranaka0505@gmail.com>
2023-09-26 17:51:22 +0700

    Fix Route

--- index.js
+++ index.js
@@ -11,34 +11,27 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("API Auction!");
+  res.send("API Auction!");
 });
-
-
 
 connectDB();
 
 app.listen(3000, () => {
-	console.log("Server is running on port 3000.");
+  console.log("Server is running on port 3000.");
 });
 
 // Route
 const AuctionRouter = require("./api/routes/Auction.js");
-const UserRouter = require("./api/routes/User.js");
-  
 const CustomerRouter = require("./api/routes/customerRoutes.js");
 const SellerRouter = require("./api/routes/sellerRoutes.js");
 const Delivery = require("./api/routes/deliveryRoutes.js");
 const Insurance = require("./api/routes/insuranceRoutes.js");
 const PaymentRouter = require("./api/routes/Payment.js");
 
-app.use("/user", UserRouter);
 app.use("/customer", CustomerRouter);
 app.use("/seller", SellerRouter);
 app.use("/payment", PaymentRouter);
 app.use("", Insurance);
-
-
+app.use("/delivery", Delivery);
 app.use("/auction", AuctionRouter);
 app.use("", Insurance);
-
