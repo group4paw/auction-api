711bb69a1225641ef8654346749e9ba692b51f0a
Aly Murry Darmoko <133185520+amurryd@users.noreply.github.com>
2023-09-27 18:29:42 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const Delivery = require("./api/routes/deliveryRoutes.js");
 const Insurance = require("./api/routes/insuranceRoutes.js");
 const PaymentRouter = require("./api/routes/Payment.js");
 const WishlistRouter = require("./api/routes/WishlistRoutes.js");
+const PaintingRouter = require("./api/routes/paintingRoutes.js")
 
 app.use("/wishlist", WishlistRouter);
 app.use("/customer", CustomerRouter);
@@ -36,3 +37,4 @@ app.use("/payment", PaymentRouter);
 app.use("/insurance", Insurance);
 app.use("/delivery", Delivery);
 app.use("/auction", AuctionRouter);
+app.use("/painting", PaintingRouter);
