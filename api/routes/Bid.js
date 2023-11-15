0a0067a1ea5a5bc4be39783731ff46cf5c170759
Farhan Franaka <farhanfranaka0505@gmail.com>
2023-11-15 19:25:02 +0700

    Fix error

--- api/routes/Bid.js
+++ api/routes/Bid.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getBidbyAuctionId } = require("../controllers/Bid.js");
+const { getAuctionByUserIdByBidder } = require("../controllers/Bid.js");
 
 router.get("/:auctionId", getBidbyAuctionId);
 router.get("/user/:userId", getAuctionByUserIdByBidder);
